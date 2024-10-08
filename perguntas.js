298b1c886fc750e047d4e1c1ea944e79d0200bb9
leleiva <162338309+leleiva@users.noreply.github.com>
2024-10-08 11:48:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,45 @@
+criaCartao(
+    'História',
+    'Em que período o fogo foi descoberto?',
+    'No período paleolítico.'
+)
+criaCartao(
+    'Matemática',
+    'Quantas casas decimais tem o numero Pi?',
+    'Ele possui infinitas casas!'
+)
+criaCartao(
+    'Geografia',
+    'Quais os países co maior e menor expectitiva de vida?',
+    'Japão (84 anos) e Serra Leoa (53 anos).'
+)
+criaCartao(
+    'Química',
+    'Atualmente,  quantos elementos químicos a tabela periódica possui??',
+    'Possui 118 elementos.'
+)
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual o numero mínimo de jogadores em cada time numa partida de futebol?',
+    '7 jogadores'
+)
+criaCartao(
+    'Ciências',
+    'Quanto tempo que a luz do Sol demora para chegar a Terra?',
+    'Demora 8 minutos'
+)
+criaCartao(
+    'Física',
+    'Qual a velocidde da luz?',
+    '299 792 458 m/s'
+)
+criaCartao(
+    'Folclore Brasileiro',
+    'Qual o personagem é agradado pelos caçadores com oferta de fumo?',
+    'Personagem Caipora.'
+)
+criaCartao(
+    'História',
+    'Qual o nome presidente do Brasil que ficou conhecido como Jango?',
+    'Presidente João Goulart'
+)

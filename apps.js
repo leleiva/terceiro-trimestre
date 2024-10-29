4783f2c81c0d3ec696300f01477555c92fd72bde
leleiva <162338309+leleiva@users.noreply.github.com>
2024-10-29 11:38:14 -0300

    Add files via upload

--- apps.js
+++ apps.js
@@ -17,7 +17,8 @@ cartao.innerHTML = `
 let respostaEstaVisivel = false
 function viraCartao(){
     respostaEstaVisivel = !respostaEstaVisivel
-    cartao.classList.toggle('active', respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+}
     cartao.addEventListener('click', viraCartao)
 container.appendChild(cartao)
 }
